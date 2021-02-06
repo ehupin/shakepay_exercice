ab58c0188a31cb8622d190a592f54957b1b5c411
ehupin <erick.hupin@gmail.com>
2021-02-06 10:55:56 -0500

    wip

--- shakepay_component.vue
+++ shakepay_component.vue
@@ -7,5 +7,17 @@
 </style>
 
 <script>
+import $ from "jquery";
 
+export default {
+  data(){
+    return {
+      historyJson: 'https://shakepay.github.io/programming-exercise/web/transaction_history.json',
+      rates: {
+        "CAD_BTC":0.00001919,
+        "BTC_CAD":52100.34,
+        "CAD_ETH":0.000466801578448052,"ETH_CAD":2142.23,"USD_BTC":0.00002447,"BTC_USD":40859.33,"USD_ETH":0.000595225694702788,"ETH_USD":1680.03,"BTC_ETH":24.31906614785992,"ETH_BTC":0.04112,"CAD_USD":0.78,"USD_CAD":1.27}
+    }
+  }
+}
 </script>
