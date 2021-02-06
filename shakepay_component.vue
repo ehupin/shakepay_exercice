4f1b45dde8ea5d5c3ba57da87760ea01a4a64ce4
ehupin <erick.hupin@gmail.com>
2021-02-06 13:01:00 -0500

    wip

--- shakepay_component.vue
+++ shakepay_component.vue
@@ -9,6 +9,7 @@
 <script>
 import $ from "jquery";
 import moment from 'moment'
+import Highcharts from 'highcharts/highstock'
 
 export default {
   data(){
@@ -31,12 +32,14 @@ export default {
         "BTC_ETH":24.31906614785992,
         "ETH_BTC":0.04112,
         "CAD_USD":0.78,
-        "USD_CAD":1.27}
+        "USD_CAD":1.27
+      },
+      ratesHistory: {}
     }
   },
   async mounted(){
+
     // store rates history in object (per pair), pre-process 'createdAt' property for faster lookup then
-    let ratesHistory = {}
     for (let pair of Object.keys(this.ratesHistoryJson)){
       const pairRatesURL = this.ratesHistoryJson[pair]
       const pairRates = await $.getJSON(pairRatesURL)
@@ -47,36 +50,10 @@ export default {
         const rateRecord = pairRates[i]
         processedPairRates.push({...rateRecord, createdAt: moment(rateRecord.createdAt).unix()})
       }
-
-      ratesHistory[pair] = processedPairRates
+      this.ratesHistory[pair] = processedPairRates
     }
 
-    function getRate(pair, dateStr, ratesHistoryPairIndex){
-
-      let lookupPair = pair
-      let reverseRate = false
-      if (!Object.keys(ratesHistory).includes(pair)){
-        const pairCurrencies = pair.split('_')
-        const reversedPair = pairCurrencies.reverse().join('_')
-        if (!Object.keys(ratesHistory).includes(reversedPair)){
-          return this.rates(pair)
-        }
-        lookupPair = reversedPair
-        reverseRate = true
-      }
-
-
-      const unixDate = moment(dateStr).unix()
-      for (const [index, entry] of ratesHistory[lookupPair].slice(ratesHistoryPairIndex).entries()){
-        if (entry.createdAt < unixDate){
-          continue
-        }
-        const rate = reverseRate ? 1/entry.midMarketRate : entry.midMarketRate
-        console.log(index)
-        return {rate, index}
-      }
-    }
-
+    // get all transactions history
     let history = (await $.getJSON(this.historyJson)).reverse()
 
     const lastRateHistoryPairIndex = {}
@@ -104,14 +81,14 @@ export default {
         // compute how much has been sold during conversion
         let fromAmount = record.from.amount
         if (record.from.currency !== this.mainCurrency){
-          let {rate, ratesHistoryPairIndex} = getRate(pair, record.createdAt, ratesHistoryPairIndex)
+          let {rate, ratesHistoryPairIndex} = this.getRate(pair, record.createdAt, ratesHistoryPairIndex)
           fromAmount *= rate
         }
 
         // compute how much has been bougth during conversion
         let toAmount = record.to.amount
         if (record.to.currency !== this.mainCurrency){
-          let {rate, ratesHistoryPairIndex} = getRate(pair, record.createdAt, ratesHistoryPairIndex)
+          let {rate, ratesHistoryPairIndex} = this.getRate(pair, record.createdAt, ratesHistoryPairIndex)
           fromAmount /= rate
         }
 
@@ -132,7 +109,7 @@ export default {
           // get history rate index for faster lookup
           let ratesHistoryPairIndex = lastRateHistoryPairIndex[pair] -1
 
-          let {rate, index} = getRate(pair, record.createdAt, ratesHistoryPairIndex)
+          let {rate, index} = this.getRate(pair, record.createdAt, ratesHistoryPairIndex)
           recordAmount *= rate
 
           // store history rate index for faster future lookups
@@ -145,6 +122,7 @@ export default {
         newAmount += (recordAmount * amountFactor)
       }
 
+      // create new grpah entry
       const newEntry = {
         x: record.createdAt,
         y: newAmount,
@@ -154,6 +132,38 @@ export default {
     }
     console.log(graphEntries)
 
+    Highcharts.stockCharts('chart')
+
+  },
+  methods: {
+    getRate(pair, dateStr, ratesHistoryPairIndex){
+
+      // as history pair rates are defined only in one way (i.e. CAD_BTC and not BTC-CAD)
+      // lets first find the right pair to lookup, and know if returned rade must be reversed
+      let lookupPair = pair
+      let reverseRate = false
+      if (!Object.keys(this.ratesHistory).includes(pair)){
+        const pairCurrencies = pair.split('_')
+        const reversedPair = pairCurrencies.reverse().join('_')
+        if (!Object.keys(this.ratesHistory).includes(reversedPair)){
+          return this.rates(pair)
+        }
+        lookupPair = reversedPair
+        reverseRate = true
+      }
+
+      // loop over rates history, starting from given index
+      // (when a rate is returned, the history index of this rate is also return to allow future request to not loop
+      // from the beginning of the history - as they are usalyy made in chronological order)
+      const unixDate = moment(dateStr).unix()
+      for (const [index, entry] of this.ratesHistory[lookupPair].slice(ratesHistoryPairIndex).entries()){
+        if (entry.createdAt < unixDate){
+          continue
+        }
+        const rate = reverseRate ? 1/entry.midMarketRate : entry.midMarketRate
+        return {rate, index}
+  }
+}
   }
 }
 </script>
